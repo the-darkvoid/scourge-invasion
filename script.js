e346eaa439fa548083a2e9802ce941576535733e
the-darkvoid <the-darkvoid@users.noreply.github.com>
2020-11-12 08:24:43 +0400

    Added map, announce and tooltips

--- script.js
+++ script.js
@@ -10,24 +10,27 @@ var rares = [
 		name: "Noth the Plaguebringer",
 		img: "images/noth-the-plaguebringer.png",
 		desc: "Noth the Plaguebringer is the first boss of the Plague Quarter of Naxxramas.",
-		way: "31.6 70.5",
+		way: {
+			x: 31.6,
+			y: 70.5
+		},
 		id: 174067,
 		loot: [
 			{
-				"id": 183642,
-				"name": "Robes of Rasped Breaths"
+				id: 183642,
+				name: "Robes of Rasped Breaths"
 			},
 			{
-				"id": 183676,
-				"name": "Hailstone Loop"
+				id: 183676,
+				name: "Hailstone Loop"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -35,28 +38,31 @@ var rares = [
 		name: "Patchwerk",
 		img: "images/patchwerk.png",
 		desc: "Patchwerk is the first boss of the Construct Quarter of Naxxramas.",
-		way: "34.4 68.5",
+		way: {
+			x: 34.4,
+			y: 68.5
+		},
 		id: 174066,
 		loot: [
 			{
-				"id": 183645,
-				"name": "Cinch of the Tortured"
+				id: 183645,
+				name: "Cinch of the Tortured"
 			},
 			{
-				"id": 183644,
-				"name": "Regurgitator's Shoulderpads"
+				id: 183644,
+				name: "Regurgitator's Shoulderpads"
 			},
 			{
-				"id": 183643,
-				"name": "Severance of Mortality"
+				id: 183643,
+				name: "Severance of Mortality"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -64,28 +70,31 @@ var rares = [
 		name: "Blood Queen Lana'thel",
 		img: "images/blood-queen-lanathel.png",
 		desc: "Blood-Queen Lana'thel is the last boss in the Crimson Hall of Icecrown Citadel.",
-		way: "49.7 32.7",
+		way: {
+			x: 49.7,
+			y: 32.7
+		},
 		id: 174065,
 		loot: [
 			{
-				"id": 183646,
-				"name": "Chestguard of Siphoned Vitality"
+				id: 183646,
+				name: "Chestguard of Siphoned Vitality"
 			},
 			{
-				"id": 183648,
-				"name": "Veincrusher Gauntlets"
+				id: 183648,
+				name: "Veincrusher Gauntlets"
 			},
 			{
-				"id": 183647,
-				"name": "Bloodspatter"
+				id: 183647,
+				name: "Bloodspatter"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -93,28 +102,31 @@ var rares = [
 		name: "Professor Putricide",
 		img: "images/professor-putricide.png",
 		desc: "Professor Putricide is the Scourge genius behind the development of all forms of blight, plague, ooze, scourge, and death delivery.",
-		way: "57.1 30.3",
+		way: {
+			x: 57.1,
+			y: 30.3
+		},
 		id: 174064,
 		loot: [
 			{
-				"id": 183649,
-				"name": "Bag of Discarded Entrails"
+				id: 183649,
+				name: "Bag of Discarded Entrails"
 			},
 			{
-				"id": 183651,
-				"name": "Chestplate of Septic Sutures"
+				id: 183651,
+				name: "Chestplate of Septic Sutures"
 			},
 			{
-				"id": 183650,
-				"name": "Miniscule Abomination in a Jar"
+				id: 183650,
+				name: "Miniscule Abomination in a Jar"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -122,28 +134,31 @@ var rares = [
 		name: "Lady Deathwhisper",
 		img: "images/lady-deathwhisper.png",
 		desc: "Lady Deathwhisper is a female lich and leader of the Cult of the Damned.",
-		way: "51.1 78.5",
+		way: {
+			x: 51.1,
+			y: 78.5
+		},
 		id: 174063,
 		loot: [
 			{
-				"id": 183641,
-				"name": "Shoulderpads of Corpal Rigidity"
+				id: 183641,
+				name: "Shoulderpads of Corpal Rigidity"
 			},
 			{
-				"id": 183653,
-				"name": "Deathwhisper Vestment"
+				id: 183653,
+				name: "Deathwhisper Vestment"
 			},
 			{
-				"id": 183655,
-				"name": "Handgrips of Rime and Sleet"
+				id: 183655,
+				name: "Handgrips of Rime and Sleet"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -151,32 +166,35 @@ var rares = [
 		name: "Skadi the Ruthless",
 		img: "images/skadi-the-ruthless.png",
 		desc: "Skadi the Ruthless is the third boss in Utgarde Pinnacle.",
-		way: "57.8 56.1",
+		way: {
+			x: 57.8,
+			y: 56.1
+		},
 		id: 174062,
 		loot: [
 			{
-				"id": 44151,
-				"name": "Reins of the Blue Proto-Drake"
+				id: 44151,
+				name: "Reins of the Blue Proto-Drake"
 			},
 			{
-				"id": 183670,
-				"name": "Skadi's Saronite Belt"
+				id: 183670,
+				name: "Skadi's Saronite Belt"
 			},
 			{
-				"id": 183656,
-				"name": "Drake Rider's Jerkin"
+				id: 183656,
+				name: "Drake Rider's Jerkin"
 			},
 			{
-				"id": 183657,
-				"name": "Skadi's Scaled Sollerets"
+				id: 183657,
+				name: "Skadi's Scaled Sollerets"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -184,28 +202,31 @@ var rares = [
 		name: "Ingvar the Plunderer",
 		img: "images/ingvar-the-plunderer.png",
 		desc: "Ingvar the Plunderer, commander of the dragon forces, is the third and last boss in Utgarde Keep.",
-		way: "52.4 52.6",
+		way: {
+			x: 52.4,
+			y: 52.6
+		},
 		id: 174061,
 		loot: [
 			{
-				"id": 183668,
-				"name": "Razor-Barbed Leather Belt"
+				id: 183668,
+				name: "Razor-Barbed Leather Belt"
 			},
 			{
-				"id": 183659,
-				"name": "Annhylde's Band"
+				id: 183659,
+				name: "Annhylde's Band"
 			},
 			{
-				"id": 183658,
-				"name": "Ingvar's Monolithic Skullcleaver"
+				id: 183658,
+				name: "Ingvar's Monolithic Skullcleaver"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -213,28 +234,31 @@ var rares = [
 		name: "Prince Keleseth",
 		img: "images/prince-keleseth.png",
 		desc: "Prince Keleseth, a darkfallen, is the first boss of Utgarde Keep.",
-		way: "54.0 44.7",
+		way: {
+			x: 54.0,
+			y: 44.7
+		},
 		id: 174060,
 		loot: [
 			{
-				"id": 50074,
-				"name": "Royal Crimson Cloak"
+				id: 50074,
+				name: "Royal Crimson Cloak"
 			},
 			{
-				"id": 183678,
-				"name": "Keleseth's Influencer"
+				id: 183678,
+				name: "Keleseth's Influencer"
 			},
 			{
-				"id": 183661,
-				"name": "Drake Stabler's Gauntlets"
+				id: 183661,
+				name: "Drake Stabler's Gauntlets"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -242,28 +266,31 @@ var rares = [
 		name: "The Black Knight",
 		img: "images/the-black-knight.png",
 		desc: "The Black Knight is the third and last boss of the Trial of the Champion.",
-		way: "64.8 22.1",
+		way: {
+			x: 64.8,
+			y: 22.1
+		},
 		id: 174059,
 		loot: [
 			{
-				"id": 183638,
-				"name": "Phantasmic Kris"
+				id: 183638,
+				name: "Phantasmic Kris"
 			},
 			{
-				"id": 183637,
-				"name": "Shoulderpads of the Notorious Knave"
+				id: 183637,
+				name: "Shoulderpads of the Notorious Knave"
 			},
 			{
-				"id": 183636,
-				"name": "Helm of the Violent Fracas"
+				id: 183636,
+				name: "Helm of the Violent Fracas"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -271,32 +298,35 @@ var rares = [
 		name: "Bronjahm",
 		img: "images/bronjahm.png",
 		desc: "Bronhahm, the Godfather of Souls, is the first boss in The Forge of Souls. He drops <a href=\"https://www.wowhead.com/item={{id}}/\" target=\"_blank\" data-wowhead=\"item=183634\">Papa's Mint Condition bag</a>.",
-		way: "70.7 38.4",
+		way: {
+			x: 70.7,
+			y: 38.4
+		},
 		id: 174058,
 		loot: [
 			{
-				"id": 183634,
-				"name": "Papa's Mint Condition Bag"
+				id: 183634,
+				name: "Papa's Mint Condition Bag"
 			},
 			{
-				"id": 183675,
-				"name": "Cold Sweat Mitts"
+				id: 183675,
+				name: "Cold Sweat Mitts"
 			},
 			{
-				"id": 183639,
-				"name": "Gaze of Bewilderment"
+				id: 183639,
+				name: "Gaze of Bewilderment"
 			},
 			{
-				"id": 183635,
-				"name": "Grieving Gauntlets"
+				id: 183635,
+				name: "Grieving Gauntlets"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -304,28 +334,31 @@ var rares = [
 		name: "Scourgelord Tyrannus",
 		img: "images/scourgelord-tyrannus.png",
 		desc: "Scourgelord Tyrannus is a human Death Knight riding the frost wyrm Rimefang.",
-		way: "47.2 66.1",
+		way: {
+			x: 47.2,
+			y: 66.1
+		},
 		id: 174057,
 		loot: [
 			{
-				"id": 183674,
-				"name": "Rimewoven Pantaloons"
+				id: 183674,
+				name: "Rimewoven Pantaloons"
 			},
 			{
-				"id": 183633,
-				"name": "Fringed Wyrmleather Leggings"
+				id: 183633,
+				name: "Fringed Wyrmleather Leggings"
 			},
 			{
-				"id": 183632,
-				"name": "Protector of Stolen Souls"
+				id: 183632,
+				name: "Protector of Stolen Souls"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -333,28 +366,31 @@ var rares = [
 		name: "Forgemaster Garfrost",
 		img: "images/forgemaster-garfrost.png",
 		desc: "Forgemaster Garfrost is a flesh giant covered with ice, whose duty is to forge saronite weapons and armor for the Scourge.",
-		way: "58.6 72.5",
+		way: {
+			x: 58.6,
+			y: 72.5
+		},
 		id: 174056,
 		loot: [
 			{
-				"id": 183666,
-				"name": "Legguards of the Frosty Fathoms"
+				id: 183666,
+				name: "Legguards of the Frosty Fathoms"
 			},
 			{
-				"id": 183631,
-				"name": "Ring of Carnelian and Sinew"
+				id: 183631,
+				name: "Ring of Carnelian and Sinew"
 			},
 			{
-				"id": 183630,
-				"name": "Garfrost's Two-Ton Bludgeon"
+				id: 183630,
+				name: "Garfrost's Two-Ton Bludgeon"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -362,28 +398,31 @@ var rares = [
 		name: "Marwyn",
 		img: "images/marwyn.png",
 		desc: "Marwyn, Arthas Menethil's second captain, is the second boss in the Halls of Reflection.",
-		way: "58.2 83.4",
+		way: {
+			x: 58.2,
+			y: 83.4
+		},
 		id: 174055,
 		loot: [
 			{
-				"id": 183687,
-				"name": "Frayed Flesh-Stitched Shoulderguards"
+				id: 183687,
+				name: "Frayed Flesh-Stitched Shoulderguards"
 			},
 			{
-				"id": 183663,
-				"name": "Sightless Capuchin of Ulmaas"
+				id: 183663,
+				name: "Sightless Capuchin of Ulmaas"
 			},
 			{
-				"id": 183662,
-				"name": "Frostsworn Rattleshirt"
+				id: 183662,
+				name: "Frostsworn Rattleshirt"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -391,28 +430,31 @@ var rares = [
 		name: "Falric",
 		img: "images/falric.png",
 		desc: "Falric, a captain for Arthas in both life and death, is the first boss in the Halls of Reflection.",
-		way: "50.2 87.9",
+		way: {
+			x: 50.2,
+			y: 87.9
+		},
 		id: 174054,
 		loot: [
 			{
-				"id": 183664,
-				"name": "Bracer of Ground Molars"
+				id: 183664,
+				name: "Bracer of Ground Molars"
 			},
 			{
-				"id": 183665,
-				"name": "Valonforth's Marred Pauldrons"
+				id: 183665,
+				name: "Valonforth's Marred Pauldrons"
 			},
 			{
-				"id": 183666,
-				"name": "Legguards of the Frosty Fathoms"
+				id: 183666,
+				name: "Legguards of the Frosty Fathoms"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -420,28 +462,31 @@ var rares = [
 		name: "The Prophet Tharon'ja",
 		img: "images/the-prophet-tharonja.png",
 		desc: "Prophet Tharon'ja, a skeletal wind serpent, is the last boss of Drak'Tharon Keep.",
-		way: "80.1 61.2",
+		way: {
+			x: 80.1,
+			y: 61.2
+		},
 		id: 174053,
 		loot: [
 			{
-				"id": 183686,
-				"name": "Breeches of the Skeletal Serpent"
+				id: 183686,
+				name: "Breeches of the Skeletal Serpent"
 			},
 			{
-				"id": 183685,
-				"name": "Phantasmic Seal of the Prophet"
+				id: 183685,
+				name: "Phantasmic Seal of the Prophet"
 			},
 			{
-				"id": 183684,
-				"name": "Tharon'ja's Protectorate"
+				id: 183684,
+				name: "Tharon'ja's Protectorate"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -449,28 +494,31 @@ var rares = [
 		name: "Novos the Summoner",
 		img: "images/novos-the-summoner.png",
 		desc: "Novos the Summoner, a lich, is the second boss of Drak'Tharon Keep.",
-		way: "77.8 66.1",
+		way: {
+			x: 77.8,
+			y: 66.1
+		},
 		id: 174052,
 		loot: [
 			{
-				"id": 183671,
-				"name": "Necromantic Wristwraps"
+				id: 183671,
+				name: "Necromantic Wristwraps"
 			},
 			{
-				"id": 183672,
-				"name": "Cuirass of Undeath"
+				id: 183672,
+				name: "Cuirass of Undeath"
 			},
 			{
-				"id": 183627,
-				"name": "Summoner's Granite Gavel"
+				id: 183627,
+				name: "Summoner's Granite Gavel"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -478,24 +526,27 @@ var rares = [
 		name: "Trollgore",
 		img: "images/trollgore.png",
 		desc: "Trollgore, a dire troll, is the first boss of Drak'Tharon Keep.",
-		way: "58.3 39.4",
+		way: {
+			x: 58.3,
+			y: 39.4
+		},
 		id: 174051,
 		loot: [
 			{
-				"id": 183669,
-				"name": "Cowl of the Rampaging Troll"
+				id: 183669,
+				name: "Cowl of the Rampaging Troll"
 			},
 			{
-				"id": 183626,
-				"name": "Troll Gorer"
+				id: 183626,
+				name: "Troll Gorer"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -503,20 +554,23 @@ var rares = [
 		name: "Krik'thir the Gatewatcher",
 		img: "images/krikthir-the-gatewatcher.png",
 		desc: "Krik'thir the Gatewatcher is the first boss of Azjol-Nerub.",
-		way: "67.5 58.0",
+		way: {
+			x: 67.5,
+			y: 58.0
+		},
 		id: 174050,
 		loot: [
 			{
-				"id": 183681,
-				"name": "Webrending Machete"
+				id: 183681,
+				name: "Webrending Machete"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -524,28 +578,31 @@ var rares = [
 		name: "Prince Taldaram",
 		img: "images/prince-taldaram.png",
 		desc: "Prince Taldaram, a vampiric darkfallen, is the second boss of Ahn'kahet: The Old Kingdom.",
-		way: "29.6 62.2",
+		way: {
+			x: 29.6,
+			y: 62.2
+		},
 		id: 174049,
 		loot: [
 			{
-				"id": 183679,
-				"name": "Taldaram's Supple Slippers"
+				id: 183679,
+				name: "Taldaram's Supple Slippers"
 			},
 			{
-				"id": 183677,
-				"name": "Blood-Drinker's Belt"
+				id: 183677,
+				name: "Blood-Drinker's Belt"
 			},
 			{
-				"id": 183625,
-				"name": "Reforged Necklace of Taldaram"
+				id: 183625,
+				name: "Reforged Necklace of Taldaram"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	},
@@ -553,20 +610,23 @@ var rares = [
 		name: "Elder Nadox",
 		img: "images/elder-nadox.png",
 		desc: "Elder Nadox is the first boss of Ahn'kahet: The Old Kingdom.",
-		way: "44.2 49.1",
+		way: {
+			x: 44.2,
+			y: 49.1
+		},
 		id: 174048,
 		loot: [
 			{
-				"id": 183673,
-				"name": "Nerubian Aegis Ring"
+				id: 183673,
+				name: "Nerubian Aegis Ring"
 			},
 			{
-				"id": 183200,
-				"name": "Pitch Black Scourgestone"
+				id: 183200,
+				name: "Pitch Black Scourgestone"
 			},
 			{
-				"id": 183616,
-				"name": "Accursed Keepsake"
+				id: 183616,
+				name: "Accursed Keepsake"
 			}
 		]
 	}
@@ -589,7 +649,13 @@ var html = `
 				</ul>
 				<div class="d-flex justify-content-between align-items-center">
 					<div class="btn-group">
-						<button type="button" class="waypoint btn btn-sm btn-outline-primary" data-clipboard-text="/way {{way}}">Way Point</button>
+						<button type="button" class="map btn btn-sm btn-outline-primary" data-clipboard-text="/run b=C_Map;b.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(118, {{way.x}}/100, {{way.y}}/100));">Map</button>
+					</div>
+					<div class="btn-group">
+						<button type="button" class="announce btn btn-sm btn-outline-primary" data-clipboard-text="/run b=C_Map;b.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(118, {{way.x}}/100, {{way.y}}/100));SendChatMessage('{{name}} at '..b.GetUserWaypointHyperlink(),'CHANNEL',_,1);">Announce</button>
+					</div>
+					<div class="btn-group">
+						<button type="button" class="tomtom btn btn-sm btn-outline-primary" data-clipboard-text="/way {{way.x}} {{way.y}}">TomTom</button>
 					</div>
 
 					<small class="text-muted">{{nextSpawnDuration}} @ {{nextSpawn}}</small>
@@ -666,6 +732,11 @@ function refreshCards() {
 	if (typeof ga !== 'undefined') {
 		ga('send', 'event', 'cards', 'refresh');
 	}
+
+	// Reset tooltips
+	$('button.map').tooltip({ title: 'Creates a map waypoint on your map pointing to the rare.', trigger: 'hover'});
+	$('button.announce').tooltip({ title: 'Creates a map waypoint and announces it linked into the zone channel.', trigger: 'hover'});
+	$('button.tomtom').tooltip({ title: 'Creates a TomTom waypoint (requires TomTom addon).', trigger: 'hover'});
 
 	return false;
 }
